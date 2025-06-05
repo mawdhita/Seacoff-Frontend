e6bc388890b5a4d5ec2a6e3549fa146805215a3c
mawar <mawar@gmail.com>
2025-06-05 08:54:30 -0700

    fixx bgt

--- src/Nota.js
+++ src/Nota.js
@@ -23,7 +23,7 @@ const Nota = () => {
         </div>
 
         <div className="nota-info"><strong>Nama:</strong> {customerName}</div>
-        <div className="nota-info"><strong>Menu:</strong> {item.nama_menu}  {quantity}</div>
+        <div className="nota-info"><strong>Menu:</strong> {menu?.nama_menu}*{quantity}</div>
         <div className="nota-info"><strong>Tanggal:</strong> {tanggal}</div>
         <div className="nota-info"><strong>Waktu:</strong> {waktu}</div>
 
