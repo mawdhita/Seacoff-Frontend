a71187bc899cd4ac938c6049af2e5a21808fa843
mawar <mawar@gmail.com>
2025-06-15 09:12:16 +0700

    apala

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'cart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 import axios from 'axios';
 import './App.css';
 
